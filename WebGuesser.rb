419ec8e14b6a33d789358fbe25cc14b55318596d
Timothy Truong, Jr <timt51@live.com>
2013-07-09 15:38:09 -0400

    Outputs message based on guess

--- WebGuesser.rb
+++ WebGuesser.rb
@@ -2,7 +2,14 @@ require "sinatra"
 require "sinatra/reloader"
 
 secret_number = rand(99) + 1
+# def number
+# 	@@secret_number ||= rand(99) + 1
+# end
 
+# def check_guess(guess)
+
+# end
 get "/" do
+	# message = check_guess(guess)
 	erb :index, :locals => {:secret_number => secret_number}
 end
