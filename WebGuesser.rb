3f361ebc496981edcd61541b84ffc58a5b2bd1a2
Timothy Truong, Jr <timt51@live.com>
2013-07-10 11:03:22 -0400

    Refactored code

--- WebGuesser.rb
+++ WebGuesser.rb
@@ -1,15 +1,30 @@
 require "sinatra"
 require "sinatra/reloader"
 
-secret_number = rand(99) + 1
-# def number
-# 	@@secret_number ||= rand(99) + 1
-# end
+SECRET_NUMBER = rand(99) + 1
 
-# def check_guess(guess)
-
-# end
+def check_guess(guess)
+	if params["guess"].nil?
+	else	
+		if guess > SECRET_NUMBER
+			if guess - 5 > SECRET_NUMBER
+				"Way too high!!!"
+			else										
+				"Too high!"
+			end
+		elsif guess < SECRET_NUMBER
+			if guess + 5 < SECRET_NUMBER	
+				"Way too low!!!"
+			else										
+				"Too low!"
+			end
+		else											
+			"You got it right! The secret number is #{SECRET_NUMBER}" 
+		end
+	end
+end
 get "/" do
-	# message = check_guess(guess)
-	erb :index, :locals => {:secret_number => secret_number}
+	guess = params["guess"].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:message => message}
 end
